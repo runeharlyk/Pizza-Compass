f17e6f2f384cc49339063029c9b5850751111b2c
Rune Harlyk <rune@harlyk.dk>
2024-08-25 13:08:34 +0200

    🪰 Adds motion store

---
+++ src/lib/stores/motion.ts
@@ -0,0 +1,30 @@
+import { browser } from '$app/environment';
+import { readable } from 'svelte/store';
+
+export const phoneHeading = readable(0, (set) => {
+	if (!browser) return;
+	let sensor: any;
+	const updateHeading = (e: any) => {
+		let alpha;
+		if (e.webkitCompassHeading) alpha = e.webkitCompassHeading;
+		else if (e.alpha) alpha = e.alpha;
+		else {
+			let q = e.target.quaternion;
+			alpha =
+				Math.atan2(2 * q[0] * q[1] + 2 * q[2] * q[3], 1 - 2 * q[1] * q[1] - 2 * q[2] * q[2]) *
+				(180 / Math.PI);
+			if (alpha < 0) alpha += 360;
+		}
+		set(alpha);
+	};
+	if ('AbsoluteOrientationSensor' in window) {
+		let sensor = new window.AbsoluteOrientationSensor({ frequency: 60 });
+		sensor.addEventListener('reading', updateHeading);
+		sensor.start();
+	} else if (window.DeviceMotionEvent) window.addEventListener('deviceorientation', updateHeading);
+
+	return () => {
+		sensor?.removeEventListener('reading', updateHeading);
+		window.addEventListener('deviceorientation', updateHeading);
+	};
+});
