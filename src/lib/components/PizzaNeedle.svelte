a8cbc58eb82b4333a5213b62a924a9130db63234
Rune Harlyk <rune@harlyk.dk>
2024-08-25 13:06:44 +0200

    🍕 Adds pizza neddle component

---
+++ src/lib/components/PizzaNeedle.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+	import pizza from '$lib/assets/pizza.png';
+	import { phoneHeading, pizzaAngle } from '$lib/stores';
+</script>
+
+<img
+	class="w-2/3 h-2/3 max-w-screen max-h-screen object-contain aspect-square"
+	src={pizza}
+	alt="pizza"
+	style="transform: rotate({-180 + $pizzaAngle + $phoneHeading}deg)"
+/>
